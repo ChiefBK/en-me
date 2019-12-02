8f7db6978a85fe40b3315e403b428d18c840a1c5
Ian Pierce <iandpierce@gmail.com>
2019-12-01 23:44:45 -0600

    cleanup

--- lib/passwords.rb
+++ lib/passwords.rb
@@ -3,7 +3,7 @@ module Passwords
     temporary_password = self.generate_password
     temp_pass = TemporaryPassword.create(user: user, password: temporary_password)
 
-    # TODO - delete jobs that already exist for given user
+    # TODO - what to do with temporary passwords if multiple are generated for same user
     DeleteTemporaryPassJob.set(wait: duration).perform_later(temp_pass.id)
     temp_pass.password
   end
@@ -17,27 +17,5 @@ module Passwords
     random_hex_string = SecureRandom.hex
     random_dec_string = Integer("0x#{random_hex_string}").to_s
     random_dec_string[0...length]
-  end
-
-  class Tracker
-    include Singleton
-
-    attr_reader :user_id_to_password
-
-    def initialize
-      @user_id_to_password = {}
-    end
-
-    def track_password(user, password)
-      @user_id_to_password[user.id] = password
-    end
-
-    def get_password(user)
-      @user_id_to_password[user.id]
-    end
-
-    def delete_password(user)
-      @user_id_to_password.delete(user.id)
-    end
   end
 end
